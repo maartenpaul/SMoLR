d6b85a011aa4ac1a020503b6895c2e3a2b4c4d90
Maarten Paul <Maarten Paul>
2016-04-24 20:35:57 +0200

    Import IJ ROIs

---
+++ R/IJROI_statistics.R
@@ -0,0 +1,14 @@
+library(RImageJROI)
+IJROI_statistics <- function(file,pxsize=5){
+  if(file_ext(file)=="zip"){
+    data <- read.ijzip(file)
+    out <- ldply(data,function(x) c(x$xrange*pxsize,x$yrange*pxsize))
+    names(out) <- c("ROI_id","ROI_xmin","ROI_xmax","ROI_ymin","ROI_ymax")
+  } else if (file_ext(file)=="roi"){
+    data <- read.ijroi(file)
+    tmp <- c(data$xrange*pxsize,data$yrange*pxsize)
+    out <- data.frame("ROI_id"=1,"ROI_xmin"=0,"ROI_xmax"=0,"ROI_ymin"=0,"ROI_ymax"=0)
+    out[1,2:5] <- tmp
+  }
+  invisible(out)
+}
