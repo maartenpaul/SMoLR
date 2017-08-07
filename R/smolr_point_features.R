ac0bd19b43aa02ccb30aaf64c27aa7f27a4f4e21
maartenpaul <maarten.paul@gmail.com>
2017-08-07 16:50:11 +0200

    Improved documentation

--- R/smolr_point_features.R
+++ R/smolr_point_features.R
@@ -1,4 +1,4 @@
-smolr_point_features <- function(x){
+smolr_point_features <- function(x,skeleton=TRUE){
   #avoid issues with capital letters
     ind_cluster <- grep("^cluster$",names(x),ignore.case=T)
     ind_x <- grep("^x$",names(x),ignore.case=T)
@@ -25,7 +25,7 @@ smolr_point_features <- function(x){
       area <- pracma::polyarea(coord[rev(chull_coord),1], coord[rev(chull_coord),2])
       perimeter <- pracma::poly_length(coord[rev(chull_coord),1], coord[rev(chull_coord),2])
       
-      
+      if(skeleton){
       xlim <- c(min(dx)-50,max(dx)+50)
       ylim <-  c(min(y)-50,max(y)+50)   
       kde <- SMOLR_KDE(x,threshold = 0.1,px=5,bandwidth = c(10,10),xlim=xlim,ylim=ylim)$kde_binary[,,1]
@@ -40,42 +40,51 @@ smolr_point_features <- function(x){
                    "sd" = ((D$sdev[1] + D$sdev[2])/2) * 2.35,"width" = (max(D$scores[,1]) - min(D$scores[,1])),"area"=area,"perimeter"=perimeter,
                    "major_axis"= D$sdev[1]*2.35,"minor_axis"= D$sdev[2]*2.35 ,"ratio" = (D$sdev[1] /D$sdev[2]),"angle" = angle,"N" = nrow(x),"skeleton"=length_skeleton
         )
-      )})
+      )
+      } else {
+        return(
+          data.frame("meanX" = mean(dx),"meanY" = mean(y),
+                     "sd" = ((D$sdev[1] + D$sdev[2])/2) * 2.35,"width" = (max(D$scores[,1]) - min(D$scores[,1])),"area"=area,"perimeter"=perimeter,
+                     "major_axis"= D$sdev[1]*2.35,"minor_axis"= D$sdev[2]*2.35 ,"ratio" = (D$sdev[1] /D$sdev[2]),"angle" = angle,"N" = nrow(x),"skeleton"=NULL
+          )
+        ) 
+    }
+      })
     
   }
   
-SMOLR_POINT_FEATURES <- function(x){
+SMOLR_POINT_FEATURES <- function(x,skeleton=TRUE){
   UseMethod("SMOLR_POINT_FEATURES")
 }
 
 
-SMOLR_POINT_FEATURES.default <- function(x){
+SMOLR_POINT_FEATURES.default <- function(x,skeleton=TRUE){
   cat("x must be data.frame or of the class smolr_dbscan or smolr_kde")
 }
 
-SMOLR_POINT_FEATURES.data.frame <- function(x){
+SMOLR_POINT_FEATURES.data.frame <- function(x,skeleton=TRUE){
   x$Cluster <- 1
-  smolr_point_features(x)
+  smolr_point_features(x,skeleton)
 }
 
-SMOLR_POINT_FEATURES.smolr_kde <- function(x){
+SMOLR_POINT_FEATURES.smolr_kde <- function(x,skeleton=TRUE){
   x <- x$int
   x$Cluster <- x$binary_no 
   x$Channel <- x$channel
-  return(smolr_point_features(x))
+  return(smolr_point_features(x,skeleton))
 }
 
-SMOLR_POINT_FEATURES.smolr_dbscan <- function(x){
+SMOLR_POINT_FEATURES.smolr_dbscan <- function(x,skeleton=TRUE){
   x <- x$dbscan
   
-  return(smolr_point_features(x))
+  return(smolr_point_features(x,skeleton))
 }
 
 
-SMOLR_POINT_FEATURES.list <- function(x){
+SMOLR_POINT_FEATURES.list <- function(x,skeleton=TRUE){
   features <- list()
   llply(x,function(x){
-    SMOLR_POINT_FEATURES(x)
+    SMOLR_POINT_FEATURES(x,skeleton)
   })
   
 }
