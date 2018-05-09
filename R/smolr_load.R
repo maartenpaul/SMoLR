9367e0ec720fadbf10e0a46ee7e37b075bf08fb3
JASlotman <j.slotman@erasmusmc.nl>
2018-05-09 15:23:37 +0200

    Commented out test for equal length statistics and localizations

--- R/smolr_load.R
+++ R/smolr_load.R
@@ -1,8 +1,14 @@
 smolr_load <- function(folder,statistics=F,prename=""){
   load(file.path(folder,paste(prename,"localizations.Rdata",sep="")),.GlobalEnv)
   if (statistics==T){
+    
     load(file.path(folder,paste(prename,"statistics.Rdata",sep="")),.GlobalEnv)
-    if(nrow(statistics)!=length(localizations)) warning("localizations and statistics not the same length")
+    
+    #if(nrow(statistics)!=length(localizations)){ 
+    #  
+    #  warning("localizations and statistics not the same length")
+    #}
+    
   }
   
 }
